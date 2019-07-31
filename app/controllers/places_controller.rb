6bfbb4e222ff6d84a783fa585874cff12d732f76
Christina Scott <christinascott11@yahoo.com>
2019-07-31 12:04:17 +0000

    make it better

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,6 @@
 class PlacesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
 
   def index
     @places = Place.all
@@ -8,4 +10,14 @@ class PlacesController < ApplicationController
      @place = Place.new
   end
 
+  def create
+    current_user.places.create(place_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
 end
