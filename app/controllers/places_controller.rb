4057428ff3848631b0c55b6d3e3e06ae1ff24edf
Christina Scott <christinascott11@yahoo.com>
2019-08-01 22:58:56 +0000

    .gitignore fix

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,7 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
 
-
+end
   def index
     @places = Place.all
   end
@@ -15,9 +15,31 @@ class PlacesController < ApplicationController
     redirect_to root_path
   end
 
+  def show
+    @place = Place.find(params[:id])
+  end
+
+  def edit
+     @place = Place.find(params[:id])
+
+     if @place.user != current_user
+    return render plain: 'Not Allowed', status: :forbidden
+  end
+
+  def update
+    @place = Place.find(params[:id])
+   if @place.user != current_user
+    return render plain: 'Not Allowed', status: :forbidden
+  end
+
+  def destroy
+    @place = Place.find(params[:id])
+    @place.destroy
+    redirect_to root_path
+  end
+
   private
 
   def place_params
     params.require(:place).permit(:name, :description, :address)
   end
-end
